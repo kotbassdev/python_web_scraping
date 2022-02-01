cebf34a55b969e97309933698611bcf1945c1b6d
kotbassdev <kotbass.dev@gmail.com>
2022-02-01 11:11:37 +0700

    add app_read_content.py code read content

---
+++ app_read_content.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://365liveball.com/%E0%B8%9E%E0%B8%A3%E0%B8%B5%E0%B9%80%E0%B8%A1%E0%B8%B5%E0%B8%A2%E0%B8%A3%E0%B9%8C%E0%B8%A5%E0%B8%B5%E0%B8%81/%E0%B9%80%E0%B8%AD%E0%B8%9F%E0%B8%95%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B9%81%E0%B8%A5%E0%B8%A1%E0%B8%9E%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%94%E0%B8%84%E0%B8%B8%E0%B8%A1%E0%B8%97%E0%B8%B1%E0%B8%9E%E0%B8%A2%E0%B8%B7%E0%B8%A1%E0%B9%80%E0%B8%94%E0%B8%AD%E0%B9%80%E0%B8%9A%E0%B9%87%E0%B8%84%E0%B8%84%E0%B8%A7%E0%B9%89%E0%B8%B2%E0%B8%AD%E0%B8%B1%E0%B8%A5%E0%B8%A5%E0%B8%B5%E0%B9%88"
+web_data = requests.get(url)
+
+soup = BeautifulSoup(web_data.text,'html.parser')
+h1 = soup.find("h1")
+print('h1:' +str(h1.text))
+
+content = soup.find("div",{"class":"content-detail-box"})
+print(content.text)
