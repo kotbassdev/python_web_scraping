536cfb37d41878ff350fb58cd0fca89c5a4ed27b
kotbassdev <kotbass.dev@gmail.com>
2022-02-01 11:40:41 +0700

    update func write data in data.txt

--- app_read_content.py
+++ app_read_content.py
@@ -5,17 +5,35 @@ url = "https://365liveball.com/%E0%B8%9E%E0%B8%A3%E0%B8%B5%E0%B9%80%E0%B8%A1%E0%
 web_data = requests.get(url)
 
 soup = BeautifulSoup(web_data.text,'html.parser')
+
+
+#Read H1
 h1 = soup.find("h1")
-# print('h1:' +str(h1.text))
+h1_text = h1.text
+print('H1: '+str(h1_text))
 
+# Write H1
+file = open("data.txt","w",encoding='utf-8')
+file.write("------H1-----\n")
+file = open("data.txt","a",encoding='utf-8')
+file.write(str(h1.text))
+file.write("\n\n")
+
+#Read Content
 content = soup.find("div",{"class":"content-detail-box"})
-# print(content.text)
+print(content.text)
 
+# Write Content
+file.write("------Content-----\n")
+file.write(content.text)
+file.close()
 
+# Read Image
 bx_detail = soup.find("div",{"class":"bx-detail"})
 img = bx_detail.find("img")
 img_src = img.get('src')
 
+# Download Image
 img_res = requests.get(img_src)
 file = open("image.jpg","wb")
 file.write(img_res.content)
