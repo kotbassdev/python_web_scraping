1cdfe4b47ccc27b99c70ebb44b210ec289e1357a
kotbassdev <kotbass.dev@gmail.com>
2022-02-01 10:59:53 +0700

    add code

---
+++ app.py
@@ -0,0 +1,13 @@
+import requests 
+from bs4 import BeautifulSoup
+
+url = "https://365liveball.com/%E0%B8%9E%E0%B8%A3%E0%B8%B5%E0%B9%80%E0%B8%A1%E0%B8%B5%E0%B8%A2%E0%B8%A3%E0%B9%8C%E0%B8%A5%E0%B8%B5%E0%B8%81"
+web_data = requests.get(url)
+# print(web_data.status_code)
+# print(web_data.text)
+
+soup = BeautifulSoup(web_data.text,'html.parser')
+file_words = soup.find_all("h2",{"class":"bx-row"})
+
+for i in file_words:
+    print(i.text)
